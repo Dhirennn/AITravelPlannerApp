b418f8175268412cf9dc0e3afeb2da8d843fcdca
Dhiren <mgur0007@student.monash.edu>
2024-12-12 00:13:24 +0800

    Add StartNewTripCard

--- ai-travel-planner-app/ai-travel-planner-app/app/(tabs)/mytrip.jsx
+++ ai-travel-planner-app/ai-travel-planner-app/app/(tabs)/mytrip.jsx
@@ -1,10 +1,53 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { Colors } from '../../constants/Colors'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import StartNewTripCard from '../../components/MyTrips/StartNewTripCard';
 
 export default function MyTrip() {
+
+  const [userTrips, setUserTrips] = useState();
+
+
   return (
-    <View>
-      <Text>MyTrip</Text>
+    <View style={{
+      padding:25,
+      paddingTop:55,
+      backgroundColor: Colors.WHITE,
+      height:'100%'
+
+
+    }}>
+
+      <View
+      
+      style={{
+        marginTop: '6%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}
+
+      >
+
+      <Text style={{
+        fontSize: 35,
+        fontFamily: 'roboto-bold'
+
+
+      }}>
+        My Trips
+      </Text>
+      <Ionicons name="add-circle" size={48} color="black" />
+
+      </View>
+
+      {userTrips?.length == undefined ? <StartNewTripCard /> : <Text>Trips</Text>}
+
+      {console.log(userTrips)}
+
+      
+      
     </View>
   )
 }
---
+++ ai-travel-planner-app/ai-travel-planner-app/components/MyTrips/StartNewTripCard.jsx
@@ -0,0 +1,57 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Colors } from './../../constants/Colors';
+
+
+export default function StartNewTripCard() {
+  return (
+    <View style={{
+        padding: 20,
+        marginTop: 50,
+        display: 'flex',
+        alignItems: 'center',
+        gap:30
+    }}>
+    <Ionicons name="location-sharp" size={30} color="black" />
+    
+    <Text style={{
+        fontSize: 25,
+        fontFamily: 'roboto-medium',
+    }}>
+    No trips planned yet
+    </Text>
+
+    <Text style={{
+        fontSize: 20,
+        fontFamily: 'roboto',
+        textAlign: 'center',
+        color: Colors.GRAY
+    }}>
+    Looks like you haven't planned any trips yet. Start by adding a new trip.
+    </Text>
+
+    <TouchableOpacity
+    
+    style={{
+        padding: 15,
+        backgroundColor: Colors.PRIMARY,
+        borderRadius: 15,
+        paddingHorizontal: 30,
+
+
+    }}    
+    
+    >
+    
+        <Text style={{
+            color: Colors.WHITE,
+            fontFamily: 'roboto-medium',
+            fontSize: 20
+        }}>Start a new trip</Text>
+    
+    </TouchableOpacity>
+
+    </View>
+  )
+}
