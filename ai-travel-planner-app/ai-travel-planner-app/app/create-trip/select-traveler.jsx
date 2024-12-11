23f16a10215700c089c8a30cfaada1f971c44e19
Dhiren <mgur0007@student.monash.edu>
2024-12-12 01:49:25 +0800

    Added different travel options

--- ai-travel-planner-app/ai-travel-planner-app/app/create-trip/select-traveler.jsx
+++ ai-travel-planner-app/ai-travel-planner-app/app/create-trip/select-traveler.jsx
@@ -1,10 +1,66 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from 'expo-router'
+import { Colors } from '../../constants/Colors';
 
 export default function SelectTraveler() {
+
+    // Options List
+
+
+
+
+    // back button
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerShown: true,
+            headerTransparent: true,
+            headerTitle: '',
+        })
+
+    }, [])
+
   return (
-    <View>
-      <Text>SelectTraveler</Text>
-    </View>
+    <View style={{
+        padding: 25,
+        paddingTop: 90,
+        backgroundColor: Colors.WHITE,
+        height: '100%',
+        marginTop: 20,
+    }}>
+      <Text
+        style={{
+            fontFamily:'roboto-bold',
+            fontSize: 36,
+            color: Colors.PRIMARY,
+        }}
+      
+      >Who's Travelling?</Text>
+    
+    <View
+    
+    style={{
+        marginTop: 20
+    }}
+    >
+        
+        <Text
+        style={{
+            fontFamily:'roboto-bold',
+            fontSize: 24,
+            color: Colors.GRAY,
+        }}>Choose your travelers</Text>
+    
+        </View>
+    
+    
+    
+      </View>
+
+
+
+
   )
 }
---
+++ ai-travel-planner-app/ai-travel-planner-app/constants/Options.js
@@ -0,0 +1,39 @@
+export const SelectTravelerList = [
+    {
+        id: 1,
+        title: 'Just Me',
+        desc: 'Traveling solo',
+        icon: '‍✈️',
+        people: '1 Person'
+
+    },
+
+    {
+        id: 2,
+        title: 'Couple',
+        desc: 'A romantic getaway?',
+        icon: '🥂',
+        people: '2 People'
+
+    },
+
+    {
+        id: 3,
+        title: 'Family',
+        desc: 'Fun for the whole family',
+        icon: '👨‍👩‍👦',
+        people: '3-5 People'
+
+    },
+
+    {
+        id: 4,
+        title: 'Friends',
+        desc: 'A bunch of thrill seekers',
+        icon: '🎉',
+        people: '6+ People'
+    },
+
+
+
+]
