70979e0e4e83fc7c3e3e17c7be458fe8f56ba2c3
Dhiren <mgur0007@student.monash.edu>
2024-12-12 02:25:43 +0800

    Updated OptionCard to include shorter descriptions

---
+++ ai-travel-planner-app/ai-travel-planner-app/components/CreateTrip/OptionCard.jsx
@@ -0,0 +1,54 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Colors } from '../../constants/Colors'
+
+export default function OptionCard({option, selectedTraveler}) {
+  return (
+    <View
+      style={[{
+        padding:25,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: Colors.LIGHT_GRAY,
+        borderRadius: 15,
+      }, selectedTraveler?.id === option?.id && {borderWidth: 3}]}
+    
+    >
+
+      <View>
+      
+        <Text
+          style={{
+            fontFamily: 'roboto-bold',
+            fontSize: 20,
+            color: Colors.PRIMARY
+          }}
+
+        >{option.title}
+        </Text>
+
+        <Text
+          style={{
+            fontFamily: 'roboto',
+            fontSize: 16,
+            color: Colors.GRAY
+          }}
+
+        >{option.desc}
+      </Text>
+      
+
+      </View>
+
+      <Text
+          style={{          
+            fontSize: 40,
+          }}
+        >{option.icon}
+      </Text>
+
+
+    </View>
+  )
+}
